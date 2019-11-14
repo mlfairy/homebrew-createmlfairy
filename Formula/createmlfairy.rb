b02c00c96148aab07a0221c3716688355335db53
MLFairy <mlfairy@vijaysharma.ca>
2019-11-14 15:08:30 -0500

    Updated path to project

--- Formula/createmlfairy.rb
+++ Formula/createmlfairy.rb
@@ -3,10 +3,10 @@
 class Createmlfairy < Formula
   desc "A CLI wrapper around CreateML"
   homepage "mlfairy.com"
-  url "https://github.com/mlfairy/createmlfairy.git",
+  url "https://github.com/mlfairy/homebrew-createmlfairy.git",
       :tag      => "0.1.0",
       :revision => "f3ca7775c2d61025d0dea2a7ad3f1a5ab0d6f01f"
-  head "https://github.com/mlfairy/createmlfairy.git"
+  head "https://github.com/mlfairy/homebrew-createmlfairy.git"
 
   depends_on :xcode => ["11.0", :build]
 
